7ba38e701e34f6c99dc4b52d813ebb09c6b1ca60
Quang Le <quang.ledinh@employmenthero.com>
2023-04-04 16:09:59 +0700

    HEHEHEE

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+const Button = ({ text }) => {
+  return <button>{text}</button>;
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Button text="Click Here" />
+        <p>Edit and save to reload.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
